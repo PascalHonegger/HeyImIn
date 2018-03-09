8692d5e8ac0a33f322dd0d6a01a558ab464d9fef
Pascal Honegger <pascal.honegger.external@atos.net>
2018-03-09 16:52:39 +0100

    Login im Frontend implementiert

--- Frontend/src/app/anonymous/login/login.component.ts
+++ Frontend/src/app/anonymous/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
 	selector: 'login',
@@ -6,4 +11,32 @@ import { Component } from '@angular/core';
 	templateUrl: './login.component.html'
 })
 export class LoginComponent {
+	public form: FormGroup;
+
+	constructor(private authServer: AuthService,
+				private snackBar: MatSnackBar,
+				private router: Router,
+				formBuilder: FormBuilder) {
+					this.form = formBuilder.group({
+						mailCtrl: ['', [Validators.required, Validators.email, Validators.maxLength(40)]],
+						passwordCtrl: ['', Validators.required]
+					});
+				}
+
+	public async login() {
+		if (!this.form.valid) {
+			return;
+		}
+
+		try {
+			await this.authServer.tryCreateSession(this.form.get('mailCtrl').value, this.form.get('passwordCtrl').value);
+			this.router.navigate([this.authServer.urlAfterLogin]);
+		} catch (err) {
+			if (err instanceof HttpErrorResponse && err.status === 401) {
+				this.snackBar.open('Ungültige Anmeldedaten', 'Ok');
+			} else {
+				console.error('Unexpected error while logging in', err);
+			}
+		}
+	}
 }
