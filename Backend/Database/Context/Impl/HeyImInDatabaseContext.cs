dfc87768a3cc8d5401d51565b8c39daf542909dd
Pascal Honegger <pascal.honegger.external@atos.net>
2018-03-09 16:52:59 +0100

    Datenbankmigration aktiviert, kaskadierendes Löschen deaktiviert

--- Backend/Database/Context/Impl/HeyImInDatabaseContext.cs
+++ Backend/Database/Context/Impl/HeyImInDatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using HeyImIn.Database.Models;
 
 // ReSharper disable UnusedAutoPropertyAccessor.Global
@@ -13,16 +14,28 @@ namespace HeyImIn.Database.Context.Impl
 
 		// Main tables
 		public virtual DbSet<User> Users { get; set; }
+
 		public virtual DbSet<Appointment> Appointments { get; set; }
+
 		public virtual DbSet<Event> Events { get; set; }
+
 		public virtual DbSet<Session> Sessions { get; set; }
 
 		// Many-To-Many relation tables
 		public virtual DbSet<AppointmentParticipation> AppointmentParticipations { get; set; }
+
 		public virtual DbSet<EventParticipation> EventParticipations { get; set; }
 
 		// Token tables
 		public virtual DbSet<PasswordReset> PasswordResets { get; set; }
+
 		public virtual DbSet<EventInvitation> EventInvitations { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			// We manually remove / update relations before deleting the referenced entry
+			modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+			modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+		}
 	}
 }
