c4d3510f560436e9f1725a63d2c60082fdb28fa5
Pascal Honegger <pascal.honegger.external@atos.net>
2018-03-09 16:58:07 +0100

    Kleine Verbesserungen im Backend

---
+++ Backend/MailNotifier/Impl/NotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using HeyImIn.Database.Models;
+
+namespace HeyImIn.MailNotifier.Impl
+{
+	public class NotificationService : INotificationService
+	{
+		public Task SendPasswordResetTokenAsync(Guid token, User recipient)
+		{
+			// TODO Implement
+			return Task.CompletedTask;
+		}
+	}
+}
