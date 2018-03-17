070cc4574fb6156f950fb85206a63ea1d538488f
Pascal Honegger <pascal.honegger@bluewin.ch>
2018-03-17 20:10:09 +0100

    Titel einer Seite überall gleich formatiert, kleine Knöpfe für kleine Bildschirme

---
+++ Frontend/src/app/shared/main-title/main-title.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'main-title',
+	styleUrls: ['./main-title.component.scss'],
+	templateUrl: './main-title.component.html'
+})
+export class MainTitleComponent {
+}
