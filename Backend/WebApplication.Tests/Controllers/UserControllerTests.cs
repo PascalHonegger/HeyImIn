d101c16c6604e60d499cdf3435f5a4e5e4634c3a
Pascal Honegger <pascal.honegger@bluewin.ch>
2018-06-15 23:52:38 +0200

    DeleteAccount getestet und korrigiert (#6)

---
+++ Backend/WebApplication.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using HeyImIn.Authentication;
+using HeyImIn.Database.Context;
+using HeyImIn.MailNotifier;
+using HeyImIn.WebApplication.Controllers;
+using HeyImIn.WebApplication.Services.Impl;
+using Moq;
+using Xunit.Abstractions;
+
+namespace HeyImIn.WebApplication.Tests.Controllers
+{
+	public partial class UserControllerTests : ControllerTestBase
+	{
+		public UserControllerTests(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		private (UserController controller, Mock<ISessionService> sessionServiceMock, Mock<IPasswordService> passwordServiceServiceMock, Mock<INotificationService> notificationServiceMock) CreateController(GetDatabaseContext getContext, int? currentUserId)
+		{
+			var passwordServiceServiceMock = new Mock<IPasswordService>(MockBehavior.Strict);
+			var sessionServiceMock = new Mock<ISessionService>(MockBehavior.Strict);
+			var notificationServiceMock = new Mock<INotificationService>(MockBehavior.Strict);
+
+			var controller = new UserController(passwordServiceServiceMock.Object, sessionServiceMock.Object, notificationServiceMock.Object, new DeleteService(), getContext, DummyLogger<UserController>(), DummyLoggerFactory())
+			{
+				ControllerContext = CurrentUserContext(currentUserId)
+			};
+
+			return (controller, sessionServiceMock, passwordServiceServiceMock, notificationServiceMock);
+		}
+	}
+}
---
+++ Backend/WebApplication.Tests/Controllers/UserControllerTests_DeleteAccount.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Threading.Tasks;
+using HeyImIn.Database.Context;
+using HeyImIn.Database.Models;
+using HeyImIn.Database.Tests;
+using HeyImIn.MailNotifier;
+using HeyImIn.MailNotifier.Models;
+using HeyImIn.WebApplication.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Moq;
+using Xunit;
+
+namespace HeyImIn.WebApplication.Tests.Controllers
+{
+	public partial class UserControllerTests
+	{
+		[Fact]
+		public async Task DeleteAccount_GivenSimpleUser_UserDeleted()
+		{
+			GetDatabaseContext getContext = ContextUtilities.CreateInMemoryContext(_output);
+			int johnDoeId;
+
+			// Arrange
+			using (IDatabaseContext context = getContext())
+			{
+				EntityEntry<User> john = context.Users.Add(ContextUtilities.CreateJohnDoe());
+
+				await context.SaveChangesAsync();
+
+				johnDoeId = john.Entity.Id;
+			}
+
+			// Act
+			(UserController controller, _, _, _) = CreateController(getContext, johnDoeId);
+			IActionResult response = await controller.DeleteAccount();
+
+			// Assert
+			Assert.IsType<OkResult>(response);
+			using (IDatabaseContext context = getContext())
+			{
+				Assert.Empty(context.Users);
+			}
+		}
+
+		[Fact]
+		public async Task DeleteAccount_GivenUserWithSimpleRelations_UserAndRelationsDeleted()
+		{
+			GetDatabaseContext getContext = ContextUtilities.CreateInMemoryContext(_output);
+			int johnDoeId;
+
+			// Arrange
+			using (IDatabaseContext context = getContext())
+			{
+				User john = ContextUtilities.CreateJohnDoe();
+
+				context.PasswordResets.Add(new PasswordReset { Requested = DateTime.UtcNow, User = john });
+				context.Sessions.Add(new Session { Created = DateTime.UtcNow, User = john });
+
+				await context.SaveChangesAsync();
+
+				johnDoeId = john.Id;
+			}
+
+			// Act
+			(UserController controller, _, _, _) = CreateController(getContext, johnDoeId);
+			IActionResult response = await controller.DeleteAccount();
+
+			// Assert
+			Assert.IsType<OkResult>(response);
+			using (IDatabaseContext context = getContext())
+			{
+				Assert.Empty(context.Users);
+				Assert.Empty(context.Sessions);
+				Assert.Empty(context.PasswordResets);
+			}
+		}
+
+		[Fact]
+		public async Task DeleteAccount_GivenUserWithEvents_UserAndEventsDeleted()
+		{
+			GetDatabaseContext getContext = ContextUtilities.CreateInMemoryContext(_output);
+			int johnDoeId;
+			int event1Id;
+			int event2Id;
+
+			// Arrange
+			using (IDatabaseContext context = getContext())
+			{
+				User john = ContextUtilities.CreateJohnDoe();
+
+				Event event1 = DummyEvent(john);
+				Event event2 = DummyEvent(john, true);
+				context.EventParticipations.Add(new EventParticipation { Event = event1, Participant = john });
+				context.EventParticipations.Add(new EventParticipation { Event = event2, Participant = john });
+
+				context.EventInvitations.Add(new EventInvitation { Event = event1, Requested = DateTime.UtcNow });
+				context.EventInvitations.Add(new EventInvitation { Event = event2, Requested = DateTime.UtcNow });
+
+				await context.SaveChangesAsync();
+
+				johnDoeId = john.Id;
+				event1Id = event1.Id;
+				event2Id = event2.Id;
+			}
+
+			// Act
+			(UserController controller, _, _, Mock<INotificationService> notificationMock) = CreateController(getContext, johnDoeId);
+
+			EventNotificationInformation notificationInformation1 = null;
+			notificationMock
+				.Setup(n => n.NotifyEventDeletedAsync(It.Is<EventNotificationInformation>(e => e.Id == event1Id)))
+				.Callback<EventNotificationInformation>(e => notificationInformation1 = e)
+				.Returns(Task.CompletedTask);
+
+			EventNotificationInformation notificationInformation2 = null;
+			notificationMock
+				.Setup(n => n.NotifyEventDeletedAsync(It.Is<EventNotificationInformation>(e => e.Id == event2Id)))
+				.Callback<EventNotificationInformation>(e => notificationInformation2 = e)
+				.Returns(Task.CompletedTask);
+
+			IActionResult response = await controller.DeleteAccount();
+
+			// Assert
+			notificationMock.Verify(n => n.NotifyEventDeletedAsync(It.Is<EventNotificationInformation>(e => e.Id == event1Id)), Times.Once);
+			notificationMock.Verify(n => n.NotifyEventDeletedAsync(It.Is<EventNotificationInformation>(e => e.Id == event2Id)), Times.Once);
+
+			AssertNotificationInformation(notificationInformation1);
+			AssertNotificationInformation(notificationInformation2);
+
+			Assert.IsType<OkResult>(response);
+			using (IDatabaseContext context = getContext())
+			{
+				Assert.Empty(context.Users);
+				Assert.Empty(context.Events);
+				Assert.Empty(context.EventParticipations);
+			}
+
+			void AssertNotificationInformation(EventNotificationInformation notificationInformation)
+			{
+				Assert.NotNull(notificationInformation);
+				Assert.NotNull(notificationInformation.Title);
+				Assert.NotNull(notificationInformation.Participations);
+				foreach (UserNotificationInformation participation in notificationInformation.Participations)
+				{
+					Assert.NotNull(participation);
+					Assert.NotNull(participation.Email);
+					Assert.NotNull(participation.FullName);
+				}
+			}
+		}
+	}
+}
