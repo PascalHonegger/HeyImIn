c65f4477f7aff68db0691fd88dc1c02c3c9841b7
Pascal Honegger <pascal.honegger@bluewin.ch>
2020-02-11 21:24:51 +0100

    Not needed { static: true } removed

--- Frontend/src/app/anonymous/reset-password/reset-password.component.ts
+++ Frontend/src/app/anonymous/reset-password/reset-password.component.ts
@@ -10,7 +10,7 @@ import { ResetPasswordClient } from '../../shared/backend-clients/reset-password
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResetPasswordComponent {
-	@ViewChild('stepper', { static: true })
+	@ViewChild('stepper')
 	public stepper: MatStepper;
 
 	public requestTokenForm: FormGroup;
